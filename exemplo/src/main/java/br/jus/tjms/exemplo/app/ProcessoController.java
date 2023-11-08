45429116da092e8dce5b97e07b5f3e8d3433663e
radrien <raphael.adrien@daimler.com>
2023-11-08 19:42:24 +0000

    Adiciona o endpoint com parametro

--- exemplo/src/main/java/br/jus/tjms/exemplo/app/ProcessoController.java
+++ exemplo/src/main/java/br/jus/tjms/exemplo/app/ProcessoController.java
@@ -5,6 +5,7 @@ import br.jus.tjms.exemplo.model.service.ProcessoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +21,10 @@ public class ProcessoController {
     @GetMapping
     public ResponseEntity<List<Processo>> obterProcessos() {
         return ResponseEntity.ok(processoService.obter());
+    }
+
+    @GetMapping("/param")
+    public String processos(@RequestParam("nome") String nome) {
+        return nome;
     }
 }
