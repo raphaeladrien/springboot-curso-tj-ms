9f251206d0595d0c13108986d026de53853bed4f
radrien <raphael.adrien@daimler.com>
2023-11-22 19:47:57 +0000

    Add dockerfile in service registry

---
+++ servicos/config/src/test/java/br/jus/tjms/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.tjms.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- servicos/nomeacao/src/main/java/br/jus/tjms/nomeacao/app/NomeacaoController.java
+++ servicos/nomeacao/src/main/java/br/jus/tjms/nomeacao/app/NomeacaoController.java
@@ -2,7 +2,9 @@ package br.jus.tjms.nomeacao.app;
 
 import br.jus.tjms.nomeacao.model.param.NomeacaoParam;
 import br.jus.tjms.nomeacao.model.service.NomeacaoServico;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,11 @@ public class NomeacaoController {
 
     private NomeacaoController(NomeacaoServico nomeacaoServico) {
         this.nomeacaoServico = nomeacaoServico;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity obterPorId(@PathParam("id") Integer id) {
+        return ResponseEntity.ok("hello");
     }
 
     @PutMapping
